2256cf1d170ce8f9bd9929758b4d4631ac0056ad
zeus <94885018+Jung-jaewoo@users.noreply.github.com>
2024-04-13 22:28:10 +0900

    Title: 토끼와 경주, Time: 2585ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240413/토끼와 경주/rabit-and-race.cpp
+++ 240413/토끼와 경주/rabit-and-race.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstring>
-#define fastio ios::sync_with_stdio(0) cout.tie(0) cin.tie(0)
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
 struct Rabbit{
